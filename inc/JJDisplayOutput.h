d71fa4ab584f6a5ae33c753baef34dc2ae3c9ec0
cba1067950 <cba1067950@yahoo.com>
2017-12-04 03:45:40 -0800

    Add files via upload

--- inc/JJDisplayOutput.h
+++ inc/JJDisplayOutput.h
@@ -2,13 +2,10 @@
 #define JJDISPLAYOUTPUT_H
 
 #include <QDialog>
-#include <QtCore>
 #include <QtGui>
-#include <QGraphicsScene>
-#include <QGraphicsPixmapItem>
+#include <QGraphicsTextItem>
 #include <QObject>
 #include <QImage>
-#include <JJInputui.h>
 
 
 namespace Ui {
@@ -20,35 +17,37 @@ class JJDisplayOutput : public QDialog
     Q_OBJECT
 
 public:
-    explicit JJDisplayOutput(QWidget *parent = 0);
+    explicit JJDisplayOutput(QWidget *parent = 0, int n = 2, int s = 3, int e = 4, int w = 5);
     ~JJDisplayOutput();
-    void initializeRoad(int n, int s, int e, int w);
+    void initBuilder(int n, int s, int e, int w);
     void step();
-    static const int maxCarImages = 5;
+    static const int maxCarImages = 3;
     static const int maxLightImages = 3;
 
 protected:
     void paintEvent(QPaintEvent *e);
 
-private slots:
-    void on_pushButton_clicked();
-
 private:
     void initCars();
-    void drawCar(QPainter *qp, int x, int y, int imgIndex, int angle);
+    void drawImage(QPainter *qp, int x, int y, int imgIndex, int angle, QImage imgArray[]);
     void drawCars(QPainter *qp);
 
     void initLights();
     void drawLight(QPainter *qp, int x, int y, int imgIndex, int angle);
     void drawLights(QPainter *qp);
 
+    void initializeRoad();
     void drawRoad(QPainter *qp);
+
     void drawText(QPainter *qp);
 
     Ui::JJDisplayOutput *ui;
 
+    //Place holders
     int m_cars[4][4];
-    int m_lights[3][3];
+    int m_lights[4][4];
+
+    //Image array allocation
     QImage carImage[maxCarImages];
     QImage lightImage[maxLightImages];
     QImage *textImage;
@@ -57,6 +56,8 @@ private:
     int m_southLane;
     int m_westLane;
     int m_eastLane;
+
+    int flow = 0;
 
     const int m_laneWidth = 50;
     const int m_laneLength = 200;
