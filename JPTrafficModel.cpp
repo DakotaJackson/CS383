0042423e76c08cf67bac1cb58ebc0d087ce8a706
James Peters <32214004+JamesPeters86@users.noreply.github.com>
2017-12-05 10:29:01 -0700

    Fixed broken ref to Car

--- JPTrafficModel.cpp
+++ JPTrafficModel.cpp
@@ -9,7 +9,7 @@
 #include "JPTrafficModelExceptions.h"
 #include "JPIntersection.h"
 #include <chrono> //for seeding
-#include "SFCar.h"
+#include "SFCarGen.h"
 
 JPTrafficModel::JPTrafficModel()
 {
@@ -27,9 +27,9 @@ JPTrafficModel::JPTrafficModel()
 	uniform = new std::uniform_real_distribution<double>(0.0, 1.0);
 
 	//todo set direction map
-	_directionMappings[0] = SFCar::DESIRE_LEFT;
-	_directionMappings[1] = SFCar::DESIRE_RIGHT;
-	_directionMappings[2] = SFCar::DESIRE_STRAIGHT;
+	_directionMappings[0] = Car::DESIRE_LEFT;
+	_directionMappings[1] = Car::DESIRE_RIGHT;
+	_directionMappings[2] = Car::DESIRE_STRAIGHT;
 }
 
 /**
